0375b582c2434b8897bb032a3ef30be5c07ef201
leflores <pflores.fisi22@gmail.com>
2022-12-10 19:16:29 -0500

    feat: setup mpa

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         chat: resolve(__dirname, 'chat/index.html'),
+        translate: resolve(__dirname, 'translate/index.html'),
       }
     }
   }
